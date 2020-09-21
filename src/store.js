e1bc420e373a0d3522d1ca3f6e48531366d10da5
juanmana <juanmana0402@gmail.com>
2020-09-21 11:35:28 +0200

    store

---
+++ src/store.js
@@ -0,0 +1,40 @@
+import { createStore,applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createSagaMiddleware from "redux-saga";
+import { createLogger } from "redux-logger";
+import { all } from "redux-saga/effects";
+import history from "./utils/helpers/history";
+
+
+
+
+const rootSaga = function*() {
+    yield all([
+    
+    ]);
+  };
+
+
+
+
+  const sagaMiddleware = createSagaMiddleware();
+  const logger = createLogger();
+
+
+
+
+
+
+
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+      applyMiddleware(sagaMiddleware,
+        
+          routerMiddleware(history)
+      )
+    )
+  );
+  sagaMiddleware.run(rootSaga);
+  export { store };
